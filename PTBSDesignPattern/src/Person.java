ea3b054fcd1f8a9d8341777184835a92104ea531
Teja Lam <tlam23@sundevils.asu.edu>
2022-10-18 00:45:16 -0700

    Successfully added java files into my git private repository

---
+++ PTBSDesignPattern/src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+	private ProductMenu theProductMenu;
+
+	private ProductMenu productMenu;
+
+	public void showLabels() {
+
+	}
+
+	public ProductMenu CreateProductMenu() {
+		return null;
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showMenu() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+}
---
+++ PTBSDesignPattern/src/ProductMenu.java
@@ -0,0 +1,17 @@
+public interface ProductMenu {
+
+	private Person person;
+
+	public void showMenu();
+
+	public void showAddButton();
+
+	public void showViewButton();
+
+	public void showRadioButton();
+
+	public abstract void showLabels();
+
+	public abstract void showComboxes();
+
+}
